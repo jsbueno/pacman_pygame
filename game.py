86b4332ad3ca6987beddbf174343e4b363b6de9c
João S. O. Bueno <gwidion@gmail.com>
2022-09-17 16:33:51 -0300

    Keyboard and movement

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import pygame
 
-
 SIZE = 800, 600
+CELL = 32
 
 FPS = 30
 
@@ -13,11 +13,25 @@ def init():
 def main():
     clock = pygame.time.Clock()
 
+    x, y = 0, 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.draw.rect(Screen, (255, 255,0), (64,64,32,32))
+            elif event.type == pygame.KEYDOWN:
+                key = event.key
+                if key == pygame.K_LEFT:
+                    x -= 1
+                elif key == pygame.K_RIGHT:
+                    x += 1
+                if key == pygame.K_UP:
+                    y -= 1
+                elif key == pygame.K_DOWN:
+                    y += 1
+                if key == pygame.K_ESCAPE:
+                    return
+
+        pygame.draw.rect(Screen, (255, 255,0), (x * CELL, y * CELL, CELL, CELL))
 
         pygame.display.update()
         clock.tick(FPS)
