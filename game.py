3b913de8669c91e8dad0419adfeba40c9d34958e
João S. O. Bueno <gwidion@gmail.com>
2022-09-17 16:22:23 -0300

    Drawing boilerplate

--- game.py
+++ game.py
@@ -3,14 +3,24 @@ import pygame
 
 SIZE = 800, 600
 
+FPS = 30
+
 def init():
     pygame.init()
     global Screen
     Screen = pygame.display.set_mode(SIZE)
 
 def main():
-    pygame.display.update()
-    pygame.time.delay(700)
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.draw.rect(Screen, (255, 255,0), (64,64,32,32))
+
+        pygame.display.update()
+        clock.tick(FPS)
 
 
 if __name__ == "__main__":
