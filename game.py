28b932a9b20ffa8c337770465edf297b62295c94
João S. O. Bueno <gwidion@gmail.com>
2022-09-18 22:49:56 -0300

    Custom wall drawing, animation direction, change size

--- game.py
+++ game.py
@@ -3,21 +3,26 @@ from pathlib import Path
 
 import pygame
 
-SIZE = 800, 600
-CELL = 32
+SIZE = 1024, 768
+CELL = 48
 
 FPS = 30
 WIDTH, HEIGHT = SIZE[0] // CELL, SIZE[1] // CELL
 
 EMPTY = " "
 WALL = "*"
+OUTSIDE = "#"
+
+WALL_COLOR = (255, 255, 255)
+BG_COLOR = (0, 0, 0)
+
 
 def init():
     global Screen, BG
     pygame.init()
     Screen = pygame.display.set_mode(SIZE)
     BG = pygame.Surface(SIZE)
-    BG.fill((0,0,0))
+    BG.fill(BG_COLOR)
 
 WALL_DIRECTIONS = {
     "down": (0, 1),
@@ -26,6 +31,10 @@ WALL_DIRECTIONS = {
 }
 
 class Map:
+
+    wall_width = CELL // 3
+    color = WALL_COLOR
+
     def __init__(self, data=None):
         self.size = WIDTH, HEIGHT
         self.data = data or ([EMPTY] * (WIDTH * HEIGHT))
@@ -60,16 +69,41 @@ class Map:
             self[WIDTH - 1, y] = WALL
 
     def __getitem__(self, pos):
+        if not (0 <= pos[0] < self.size[0]) or not (0 <= pos[1] < self.size[1]):
+            return OUTSIDE
         return self.data[pos[0] + pos[1] * self.size[0]]
 
     def __setitem__(self, pos, value):
         self.data[pos[0] + pos[1] * self.size[0]] = value
 
+
     def draw(self):
+        w1 = self.wall_width
+        w2 = self.wall_width // 2
+        w3 = 0 #self.wall_width // 3
+        c2 = CELL // 2
         for y in range(self.size[1]):
             for x in range(self.size[0]):
-                if self[x, y] == WALL:
-                    pygame.draw.rect(Screen, (255, 255, 255), (x * CELL, y * CELL, CELL, CELL))
+                if self[x, y] != WALL:
+                    continue
+
+                sx, sy = CELL * x + c2, CELL * y + c2
+                middle = (sx - w2 - w3, sy - w2 - w3, w1 + 2 * w3, w1 + 2 * w3)
+                segments = []
+                if self[x - 1, y] == WALL:
+                    segments.append((sx - c2, sy - w2, c2, w1))
+                if self[x + 1, y] == WALL:
+                    segments.append((sx, sy - w2, c2, w1))
+                if self[x, y - 1] == WALL:
+                    segments.append((sx - w2, sy - c2, w1, c2))
+                if self[x , y + 1] == WALL:
+                    segments.append((sx - w2, sy, w1, c2))
+
+                pygame.draw.ellipse(Screen, self.color, middle)
+                for segment in segments:
+                    pygame.draw.rect(Screen, self.color, segment)
+
+                # pygame.draw.rect(Screen, (255, 255, 255), (x * CELL, y * CELL, CELL, CELL))
 
 
 
@@ -80,6 +114,7 @@ class Character(pygame.sprite.Sprite):
         self.map = map
         self.ox, self.oy = self.x, self.y = initial_pos or (0,0)
         self.vx, self.vy = 0, 0
+        self.ovx = 0
         self.load_assets()
 
         self.tick = 0
@@ -103,8 +138,12 @@ class Character(pygame.sprite.Sprite):
             else:
                 break
             i += 1
-        self.images = image_list
-
+        self.images = {(0, 0): image_list}
+        for i, direction in enumerate([(1, 0), (0, -1), (-1, 0), (0, 1)]):
+            self.images[direction] = [
+                pygame.transform.rotozoom(img, i * 90, 1)  if i != 2 else pygame.transform.flip(img, True, False)
+                for img in image_list
+            ]
 
     @property
     def rect(self):
@@ -112,15 +151,17 @@ class Character(pygame.sprite.Sprite):
 
     @property
     def image(self):
-        return self.images[self.tick // self.anim_cycle % 2]
+        return self.images[self.ovx, self.vy if self.ovx == 0 else 0][self.tick // self.anim_cycle % 2]
 
     def move_event(self, event):
         if event.type == pygame.KEYDOWN:
             key = event.key
             if key == pygame.K_LEFT:
                 self.vx = -1
+                self.ovx = -1
             elif key == pygame.K_RIGHT:
                 self.vx = 1
+                self.ovx = 0
             if key == pygame.K_UP:
                 self.vy = -1
             elif key == pygame.K_DOWN:
